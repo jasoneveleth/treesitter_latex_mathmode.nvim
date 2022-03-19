593573f88ce31aef9b0768c975321ee113052498
Martin Kunz <martinkunz@email.cz>
2022-03-19 12:03:57 +0100

    edit(refactor): remove unnecessary indent

--- lua/ts-latex-mode.lua
+++ lua/ts-latex-mode.lua
@@ -27,51 +27,54 @@ local function get_node_at_cursor()
   local cursor_range = { cursor[1] - 1, cursor[2] }
   local buf = vim.api.nvim_get_current_buf()
   local ok, parser = pcall(ts.get_parser, buf, 'latex')
-  if not ok or not parser then return end
+  if not ok or not parser then
+    return
+  end
   local root_tree = parser:parse()[1]
   local root = root_tree and root_tree:root()
 
-  if not root then return end
+  if not root then
+    return
+  end
 
   return root:named_descendant_for_range(cursor_range[1], cursor_range[2], cursor_range[1], cursor_range[2])
 end
 
 function M.in_comment()
-  if has_treesitter then
-    local node = get_node_at_cursor()
-    while node do
-      if node:type() == 'comment' then
-        return true
-      end
-      node = node:parent()
-    end
-    return false
+  if not has_treesitter then
+    return
   end
+  local node = get_node_at_cursor()
+  while node do
+    if node:type() == 'comment' then
+      return true
+    end
+    node = node:parent()
+  end
+  return false
 end
 
 function M.in_mathzone()
-  if has_treesitter then
-    local buf = vim.api.nvim_get_current_buf()
-    local node = get_node_at_cursor()
-    while node do
-      if MATH_NODES[node:type()] then
-        return true
-      end
-      if node:type() == 'environment' then
-        local begin = node:child(0)
-        local names = begin and begin:field('name')
+  if not has_treesitter then
+    return
+  end
+  local buf = vim.api.nvim_get_current_buf()
+  local node = get_node_at_cursor()
+  while node do
     if MATH_NODES[node:type()] ~= nil then
       return MATH_NODES[node:type()]
     end
+    if node:type() == 'environment' then
+      local begin = node:child(0)
+      local names = begin and begin:field 'name'
 
-        if names and names[1] and MATH_ENVIRONMENTS[query.get_node_text(names[1], buf):gsub('[%s*]', '')] then
-          return true
-        end
+      if names and names[1] and MATH_ENVIRONMENTS[query.get_node_text(names[1], buf):gsub('[%s*]', '')] then
+        return true
       end
-      node = node:parent()
     end
-    return false
+    node = node:parent()
   end
+  return false
 end
 
 return M
